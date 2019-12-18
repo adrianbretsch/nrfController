319323462df1333669ac72e5bfd3483a2f7449ac
bretscha <adrian.bretschneider@mailbox.tu-dresden.de>
2019-12-18 17:47:16 +0100

    trying to write csv

--- Controller.py
+++ Controller.py
@@ -1,52 +1,79 @@
+import csv
+
 import serial
+import pandas as pd
+
 
 # Global Variables
-ser = 0
+
+def parse_to(row):
+    t = open('employee_file2.csv', 'a')
+    with t as file:
+        file.write(row)
+        print(file)
 
 
-# Function to Initialize the Serial Port
-def init_serial(port) -> serial:
-    global ser  # Must be declared in Each Function
-    ser = serial.Serial()
-    ser.baudrate = 115200
-    ser.port = port  # If Using Linux
-    # Specify the TimeOut in seconds, so that SerialPort
-    # Doesn't hangs
-    ser.timeout = 1
-    ser.open()  # Opens SerialPort
+class Controller:
+    def __init__(self, port):
+        self._port = port
+        self.ser = serial.Serial()
+        self.ser.baudrate = 115200
+        self.ser.port = port  # If Using Linux
+        # Specify the TimeOut in seconds, so that SerialPort
+        # Doesn't hangs
+        self.ser.timeout = 1
+        self.ser.open()  # Opens SerialPort
 
-    # print port open or closed
-    if ser.isOpen():
-        print("Open: ' + ser.portstr")
-    return ser
+        # print port open or closed
+        if self.ser.isOpen():
+            print("Open: ' + ser.portstr")
 
-
-def console(__ser: serial.Serial):
-    while 1:
-        __ser.reset_input_buffer()
-        temp = input(" > ") + "\r\n"
-        if "exit" in temp:
-            break
-        __ser.write(temp.encode('ascii'))  # Writes to the SerialPort
-        line = __ser.readline().decode('ascii')  # Read from Serial Port
+    def console(self):
         while 1:
-            next_line = __ser.readline().decode('ascii')  # Read from Serial Port
+            self.ser.reset_input_buffer()
+            temp = input(" > ") + "\r\n"
+            if "exit" in temp:
+                break
+            self.ser.write(temp.encode('ascii'))  # Writes to the SerialPort
+            line = self.ser.readline().decode('ascii')  # Read from Serial Port
+            while 1:
+                next_line = self.ser.readline().decode('ascii')  # Read from Serial Port
+                if (line == "> \r\n" and next_line == '> ') or next_line == line:
+                    break
+                if not line == "> \r\n":
+                    print(line, end="")  # Print What is Read from Port
+                line = next_line
+        exit()
+
+    def ping_all(self, ):
+        self.ser.reset_input_buffer()
+        self.ser.write("\r\n ping ff03::1 \r\n".encode('ascii'))
+        lines = self.ser.readall().decode('ascii')
+        for line in lines:
+            line = line.strip()
+            print(line)
+
+        """
+        while 1:
+            next_line = self.ser.readline().decode('ascii')  # Read from Serial Port
             if (line == "> \r\n" and next_line == '> ') or next_line == line:
                 break
             if not line == "> \r\n":
-                print('nRF: ' + line, end="")  # Print What is Read from Port
+                print(line, end="")  # Print What is Read from Port
             line = next_line
-    exit()
+        """
 
+    def ping(self, ipaddr, size, count, interval, hoplimit):
+        self.ser.reset_input_buffer()
 
-def ping_all(__ser: serial.Serial):
-    __ser.reset_input_buffer()
-    __ser.write("\r\n ping ff03::1 \r\n".encode('ascii'))
-    line = __ser.readline().decode('ascii')
-    while 1:
-        next_line = __ser.readline().decode('ascii')  # Read from Serial Port
-        if (line == "> \r\n" and next_line == '> ') or next_line == line:
-            break
-        if not line == "> \r\n":
-            print(line, end="")  # Print What is Read from Port
-        line = next_line
+        self.ser.write("ping {} {} {} {} {}".format(ipaddr, size, count, interval, hoplimit).encode('ascii'))
+        line = self.ser.readline().decode('ascii')  # Read from Serial Port
+        while 1:
+
+            next_line = self.ser.readline().decode('ascii')  # Read from Serial Port
+            if (line == "> \r\n" and next_line == '> ') or next_line == line:
+                break
+            if not line == "> \r\n":
+                print(line, end="")  # Print What is Read from Port
+                parse_to()
+            line = next_line
--- Workflow.py
+++ Workflow.py
@@ -1,8 +1,12 @@
 import Controller
 
-serial = Controller.init_serial("/dev/ttyACM0")
+controller = Controller
+"controller.console()"
+Controller.parse_to("Michael Palin,05/23/13,66000.0,8")
+"controller.ping('ff03::1', 100, 100, 1, 1)"
 """
+pings = controller.ping_all()
+
 TODO: Do this every ... interval
 """
-Controller.ping_all(serial)
 
